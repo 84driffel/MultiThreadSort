b8b6eced7eff96b408d93f6207112277ac3f504d
Daniel Riffel <daniel-riffel@uiowa.edu>
2021-09-27 10:43:48 -0500

    Added multithreading and timing

--- class/src/edu/uiowa/mc/hw1/MergeSort.java
+++ class/src/edu/uiowa/mc/hw1/MergeSort.java
@@ -1,97 +1,164 @@
-class MergeSort
-{
-	// Merges two subarrays of arr[].
-	// First subarray is arr[l..m]
-	// Second subarray is arr[m+1..r]
-	void merge(int arr[], int l, int m, int r)
-	{
-		// Find sizes of two subarrays to be merged
-		int n1 = m - l + 1;
-		int n2 = r - m;
+import java.io.*;
+import java.util.ArrayList;
 
-		/* Create temp arrays */
-		int L[] = new int[n1];
-		int R[] = new int[n2];
 
-		/*Copy data to temp arrays*/
-		for (int i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
-		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
+class MergeSort {
+    private static int threads = 4;
 
-		/* Merge the temp arrays */
+    private static class SortingThreads extends Thread {
+        SortingThreads(int[] arr, int l, int r) {
+            super(() -> {
+                MergeSort.sort(arr, l, r);
+            });
+            this.start();
+        }
+    }
 
-		// Initial indexes of first and second subarrays
-		int i = 0, j = 0;
+    public static void threadsSorting(int[] array) {
+        long time = System.currentTimeMillis();
+        final int length = array.length;
+        boolean exact = length % threads == 0;
+        int maxlim;
+        if(exact){
+            maxlim = length/threads;
+        }else {
+            maxlim = length/(threads-1);
+        }
+        maxlim = Math.max(maxlim, threads);
+        final ArrayList<SortingThreads> threads = new ArrayList<>();
+        for (int i = 0; i < length; i += maxlim) {
+            int l = i;
+            int remain = (length) - i;
+            int r = remain < maxlim ? i + (remain - 1) : i + (maxlim - 1);
+            final SortingThreads t = new SortingThreads(array, l, r);
+            threads.add(t);
+        }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException ignored) {
+            }
+        }
+        for (int i = 0; i < length; i += maxlim) {
+            int mid = i == 0 ? 0 : i - 1;
+            int remain = (length) - i;
+            int end = remain < maxlim ? i + (remain - 1) : i + (maxlim - 1);
+            merge(array, 0, mid, end);
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Time spent for custom multi-threaded recursive merge_sort(): "+ time+ "ms");
+    }
 
-		// Initial index of merged subarray array
-		int k = l;
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
-			}
-			else {
-				arr[k] = R[j];
-				j++;
-			}
-			k++;
-		}
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    public static void merge(int arr[], int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-		/* Copy remaining elements of L[] if any */
-		while (i < n1) {
-			arr[k] = L[i];
-			i++;
-			k++;
-		}
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
 
-		/* Copy remaining elements of R[] if any */
-		while (j < n2) {
-			arr[k] = R[j];
-			j++;
-			k++;
-		}
-	}
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
-	// Main function that sorts arr[l..r] using
-	// merge()
-	void sort(int arr[], int l, int r)
-	{
-		if (l < r) {
-			// Find the middle point
-			int m =l+ (r-l)/2;
+        /* Merge the temp arrays */
 
-			// Sort first and second halves
-			sort(arr, l, m);
-			sort(arr, m + 1, r);
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
 
-			// Merge the sorted halves
-			merge(arr, l, m, r);
-		}
-	}
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
 
-	/* A utility function to print array of size n */
-	static void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
 
-	// Driver code
-	public static void main(String args[])
-	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 
-		System.out.println("Given Array");
-		printArray(arr);
+    // Main function that sorts arr[l..r] using
+    // merge()
+    public static void sort(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
 
-		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.length - 1);
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
 
-		System.out.println("\nSorted array");
-		printArray(arr);
-	}
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver code
+    public static void main(String args[]) throws IOException {
+        FileInputStream fis = new FileInputStream("array.bin");
+        DataInputStream dis = new DataInputStream(fis);
+        StringBuilder str = new StringBuilder();
+        try {
+            long index = 0;
+            while (true) {
+                long l = dis.readLong();
+                //System.out.println(index + " " + l);
+                str.append(l + " ");
+                index += 1;
+            }
+        } catch (EOFException e) {
+            fis.close();
+        }
+
+        String[] splitted = str.toString().split("\\s+");
+        int arr[] = new int[splitted.length];
+        for (int i = 0; i < splitted.length; i++) {
+            arr[i] = Integer.parseInt(splitted[i]);
+
+        }
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+
+        MergeSort.threadsSorting(arr);
+
+        printArray(arr);
+    }
 }
-
